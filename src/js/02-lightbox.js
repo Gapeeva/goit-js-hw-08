ab1606aeae2535b2eb2b917c7df64869793721d7
Gapeeva <122734035+Gapeeva@users.noreply.github.com>
2023-10-20 09:25:24 +0300

    first commit

---
+++ src/js/02-lightbox.js
@@ -0,0 +1,24 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+function saveCurrentTime(player) {
+  player.getCurrentTime().then(time => {
+    localStorage.setItem('videoplayer-current-time', time);
+  });
+}
+
+function setSavedTime(player) {
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+
+  if (savedTime) {
+    player.setCurrentTime(savedTime);
+  }
+}
+
+const player = new Player('vimeo-player');
+
+const saveCurrentTimeThrottled = throttle(() => saveCurrentTime(player), 1000);
+
+player.on('timeupdate', saveCurrentTimeThrottled);
+
+window.addEventListener('load', () => setSavedTime(player));
